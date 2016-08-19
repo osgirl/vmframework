1b7d73221c930bb83eb6353e40a62f1a68578d39
Phill Gordon <phillipgordon4@gmail.com>
2016-08-19 16:41:05 +0100

    Updating page structure

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,5 +12,19 @@
 	  });
 
 
+	  var slideRightCSS = new Menu({
+	    wrapper: '#o-wrapper',
+	    type: 'slide-right',
+	    menuOpenerClass: '.c-button',
+	    maskId: '#c-mask'
+	  });
+	  var slideRightCSSBtn = document.querySelector('#c-button--slide-right-css');
+	  
+	  slideRightCSSBtn.addEventListener('click', function(e) {
+	    e.preventDefault;
+	    slideRight.open();
+	  });
+
+
 
 
