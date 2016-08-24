05baec9c974ba635e6cd004af9f2242cbaefa654
Phill Gordon <phillipgordon4@gmail.com>
2016-08-24 16:55:27 +0100

    styling amends

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,4 @@
-	var slideRight = new Menu({
-	    wrapper: '#o-wrapper',
-	    type: 'slide-right',
-	    menuOpenerClass: '.c-button',
-	    maskId: '#c-mask'
-	  });
-	  var slideRightBtn = document.querySelector('#c-button--slide-right');
-	  
-	  slideRightBtn.addEventListener('click', function(e) {
-	    e.preventDefault;
-	    slideRight.open();
-	  });
-
-
-	  var slideRightCSS = new Menu({
-	    wrapper: '#o-wrapper',
-	    type: 'slide-right',
-	    menuOpenerClass: '.c-button',
-	    maskId: '#c-mask'
-	  });
-	  var slideRightCSSBtn = document.querySelector('#c-button--slide-right-css');
-	  
-	  slideRightCSSBtn.addEventListener('click', function(e) {
-	    e.preventDefault;
-	    slideRight.open();
-	  });
+	
 
 
 
